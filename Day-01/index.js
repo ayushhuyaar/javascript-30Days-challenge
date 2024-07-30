6d216b307e81362ba6fd9b44c772ef5d20ed6ec6
ayushhuyaar <ayushhuyaar@gmail.com>
2024-07-30 14:46:20 +0530

    day 01 tast completed

---
+++ Day-01/index.js
@@ -0,0 +1,53 @@
+//==== Day - 01 -> Variable and Data types ======//
+
+
+// Activity - 01 
+//Task - 01
+var myNumber = 10;
+console.log(myNumber);   // ouptut = 10
+
+// Task - 02
+
+let myString = "Js 30 Days challenge";
+console.log(myString);    // output = Js 30 Days challenge
+
+// Activity - 02
+
+const booleanValue = true;
+console.log(booleanValue);   // output = true; 
+
+//Activity - 03 
+
+const typeNumber = 10;
+console.log(typeof typeNumber);     // ouput  = number;
+
+const typeString = "Printing Typeof Operator";
+
+console.log(typeof typeString);    // output = string ; 
+
+const typeArray = ["ayush" , "mohan " , 3 , {name : "sohan"}];
+console.log(typeof typeArray);    // output = object 
+
+const typeBoolean = false;
+console.log(typeof typeBoolean);     // output = boolean 
+
+const typeObject = {name : "ayush" , age : 20};
+console.log(typeof typeObject);    // output = object
+
+//Activity - 04
+
+let anyNumber = 100;
+console.log(anyNumber);  // output = 100
+
+anyNumber = -25;
+console.log(anyNumber);  // output = -25
+
+
+// Activiy - 05 
+
+const firstNumber = 34;
+console.log(firstNumber);  // output = 34
+
+
+firstNumber = 56;
+console.log(firstNumber); // ouput = (error) Assignment to constant variable
